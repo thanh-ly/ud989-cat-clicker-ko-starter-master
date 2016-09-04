36d6d8f38c40e89b560a9fdb2e933e8003e2c248
thanh-ly <thanh.ly.1992@gmail.com>
2016-09-03 22:14:16 -0500

    separated model and initial data, created initialCats, pushed into array and set up currentCat

--- js/app.js
+++ js/app.js
@@ -1,9 +1,42 @@
-var Cat = function() {
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	this.imgAttribution = ko.observable('https://www.flickr.com');
-	this.nickNames = ko.observableArray (["Orange", "Catty", "Poopy"])
+var initialCats = [
+	{
+		clickCount: 0,
+		name: 'Tabby',
+		imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+		imgAttribution: 'https://www.flickr.com',
+		nickNames: ["Orange", "Catty", "Poopy"]
+	}, {
+		clickCount: 0,
+		name: 'Meowy',
+		imgSrc: 'img/22252709_010df3379e_z.jpg',
+		imgAttribution: 'https://www.flickr.com',
+		nickNames: ["Grry", "Prancy", "Poopy"]
+	}, {
+		clickCount: 0,
+		name: 'Kitty',
+		imgSrc: 'img/1413379559_412a540d29_z.jpg',
+		imgAttribution: 'https://www.flickr.com',
+		nickNames: ["Loopy", "Woobbly", "Poopy"]
+	}, {
+		clickCount: 0,
+		name: 'Tigery',
+		imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+		imgAttribution: 'https://www.flickr.com',
+		nickNames: ["Meany", "Salty", "Poopy"]
+	}, {
+		clickCount: 0,
+		name: 'Sleepy',
+		imgSrc: 'img/9648464288_2516b35537_z.jpg',
+		imgAttribution: 'https://www.flickr.com',
+		nickNames: ["Zzzzzy", "Lolly", "Poopy"]
+	}]
+
+var Cat = function(data) {
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgAttribution);
+	this.nickNames = ko.observableArray (data.nickNames)
 
 
 	//this assigns a string that activates when clickCount reaches a certain amount
@@ -21,7 +54,14 @@ var Cat = function() {
 
 var ViewModel = function() {
 	var self = this;
-	this.currentCat = ko.observable( new Cat() );
+	//creates an empty array to store cats
+	this.catList = ko.observableArray([]);
+	//for each item in the initialCats array, we push into the Cat class, which we push into the catList
+	initialCats.forEach(function(catItem) {
+		self.catList.push( new Cat(catItem) );
+	});
+	//then we set the curentCat to the first cat in the catList
+	this.currentCat = ko.observable( this.catList()[0] );
 	//uses var self
 	this.incrementCounter = function () {
 		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
